4f59e72158d8ac9d44711d70fb60803d67d7536e
Gustavo Wolfi <gmeira.antonio@gmail.com>
2022-06-04 20:46:30 -0300

    build: Creating middleware that validates a user that will login

---
+++ src/middlewares/ValidateUserLogin.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from 'express';
+import BadRequestError from '../errors/BadRequestError';
+
+const validatePassword = (password: string) => {
+  if (password === '' || password === undefined) {
+    throw new BadRequestError('All fields must be filled');
+  }
+};
+
+const validateEmail = (email: string) => {
+  if (email === '' || email === undefined) throw new BadRequestError('All fields must be filled');
+  console.log('email', email);
+  if (!email.includes('@')) throw new BadRequestError('Email must be valid');
+};
+
+const validateUserLogin = (req: Request, _res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+  validatePassword(password);
+  validateEmail(email);
+  next();
+};
+
+export default validateUserLogin;
