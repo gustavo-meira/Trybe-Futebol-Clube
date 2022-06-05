21c2e3aa5f69345a201614199902bc5a915fe52f
Gustavo Wolfi <gmeira.antonio@gmail.com>
2022-06-04 23:03:07 -0300

    feat: Creating login validate method for login controller

--- src/controllers/ILoginController.ts
+++ src/controllers/ILoginController.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 
 interface ILoginController {
   login(req: Request, res: Response, next: NextFunction): Promise<void>;
+  loginValidate(req: Request, res: Response, next: NextFunction): Promise<void>;
 }
 
 export default ILoginController;
--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -18,6 +18,16 @@ class LoginController implements ILoginController {
       next(err);
     }
   }
+
+  async loginValidate(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const token = req.headers.authorization;
+      const role = await this.loginService.loginValidate(token as string);
+      res.status(200).json(role);
+    } catch (err) {
+      next(err);
+    }
+  }
 }
 
 export default LoginController;
