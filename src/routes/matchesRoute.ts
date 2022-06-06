7a30744d28f4c008cd886275d35f951285a97e97
Gustavo Wolfi <gmeira.antonio@gmail.com>
2022-06-06 12:31:20 -0300

    feat: Create a route post to create a match

--- src/routes/matchesRoute.ts
+++ src/routes/matchesRoute.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response, Router } from 'express';
+import validateMatch from '../middlewares/ValidateMatch';
 import MatchesController from '../controllers/MatchesController';
 import MatchesService from '../services/MatchesService';
+import validateToken from '../middlewares/validateToken';
 
 const matchesRoutes = Router();
 
@@ -10,5 +12,15 @@ const matchesController = new MatchesController(matchesService);
 matchesRoutes.get('/', (req: Request, res: Response, next: NextFunction) => {
   matchesController.getAll(req, res, next);
 });
+
+matchesRoutes.use(validateToken);
+
+matchesRoutes.post(
+  '/',
+  validateMatch,
+  (req: Request, res: Response, next: NextFunction) => {
+    matchesController.create(req, res, next);
+  },
+);
 
 export default matchesRoutes;
