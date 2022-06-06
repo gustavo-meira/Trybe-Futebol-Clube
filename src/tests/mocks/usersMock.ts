bb24735b8698d1ce262ef500d0887a3bec6432fa
Gustavo Wolfi <gmeira.antonio@gmail.com>
2022-06-06 17:50:27 -0300

    tests: Creating a mock for users test

---
+++ src/tests/mocks/usersMock.ts
@@ -0,0 +1,11 @@
+const validUser = {
+  id: 1,
+  username: 'test',
+  password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
+  email: 'admin@admin.com',
+  role: 'admin',
+};
+
+export {
+  validUser,
+};
