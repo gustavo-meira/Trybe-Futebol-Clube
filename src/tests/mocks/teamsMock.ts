741823a4e302aa3d3357db529d4be1e27d2dbe38
Gustavo Wolfi <gmeira.antonio@gmail.com>
2022-06-10 06:24:55 -0300

    test: Creating a mock for teams

---
+++ src/tests/mocks/teamsMock.ts
@@ -0,0 +1,16 @@
+const teams = [
+  {
+    id: 1,
+    teamName: 'Team 1',
+  },
+  {
+    id: 2,
+    teamName: 'Team 2',
+  },
+  {
+    id: 3,
+    teamName: 'Team 3',
+  },
+];
+
+export { teams };
