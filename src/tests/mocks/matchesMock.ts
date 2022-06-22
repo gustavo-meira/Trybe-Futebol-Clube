90fe2c4b88199a6f49c541f48c4d9a514e552585
Gustavo Meira <gmeira.antonio@gmail.com>
2022-06-22 08:38:59 -0300

    test: Creating a mock for matches

---
+++ src/tests/mocks/matchesMock.ts
@@ -0,0 +1,92 @@
+export const allMatches = [
+  {
+    id: 1,
+    homeTeam: 16,
+    homeTeamGoals: 1,
+    awayTeam: 8,
+    awayTeamGoals: 1,
+    inProgress: false,
+    teamHome: {
+      teamName: 'São Paulo'
+    },
+    teamAway: {
+      teamName: 'Grêmio'
+    }
+  },
+  {
+    id: 2,
+    homeTeam: 9,
+    homeTeamGoals: 1,
+    awayTeam: 14,
+    awayTeamGoals: 1,
+    inProgress: true,
+    teamHome: {
+      teamName: 'Internacional'
+    },
+    teamAway: {
+      teamName: 'Santos'
+    }
+  },
+  {
+    id: 3,
+    homeTeam: 4,
+    homeTeamGoals: 3,
+    awayTeam: 11,
+    awayTeamGoals: 0,
+    inProgress: false,
+    teamHome: {
+      teamName: 'Corinthians'
+    },
+    teamAway: {
+      teamName: 'Napoli-SC'
+    }
+  },
+];
+
+export const matchesInProgress = [
+  {
+    id: 2,
+    homeTeam: 9,
+    homeTeamGoals: 1,
+    awayTeam: 14,
+    awayTeamGoals: 1,
+    inProgress: true,
+    teamHome: {
+      teamName: 'Internacional'
+    },
+    teamAway: {
+      teamName: 'Santos'
+    }
+  },
+];
+
+export const matchesNotInProgress = [
+  {
+    id: 1,
+    homeTeam: 16,
+    homeTeamGoals: 1,
+    awayTeam: 8,
+    awayTeamGoals: 1,
+    inProgress: false,
+    teamHome: {
+      teamName: 'São Paulo'
+    },
+    teamAway: {
+      teamName: 'Grêmio'
+    }
+  },
+  {
+    id: 3,
+    homeTeam: 4,
+    homeTeamGoals: 3,
+    awayTeam: 11,
+    awayTeamGoals: 0,
+    inProgress: false,
+    teamHome: {
+      teamName: 'Corinthians'
+    },
+    teamAway: {
+      teamName: 'Napoli-SC'
+    }
+  },
+];
